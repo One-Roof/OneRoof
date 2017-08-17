1c37591367f6a67c623daae59eddac834e7bd051
Matt McDonald <mattmcd1221@gmail.com>
2017-08-17 09:37:29 -0400

    rearrange folder structure

---
+++ src/Components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import '../Styles/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>It's One Roof Ya'll!!</h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
