f64d15fcc0bac3515f2f4b6644d85c18001a5c5b
Sam Caldwell <caldwell.s.thorne@gmail.com>
2017-08-25 15:14:16 -0400

    add form for login and signup.

--- src/Components/LoginSignUp.jsx
+++ src/Components/LoginSignUp.jsx
@@ -5,6 +5,48 @@ class LoginSignUp extends Component {
     return (
       <div className="">
         <h1>Login or Sign Up Here!</h1>
+
+        <form>
+            <h1>Login</h1>
+          <label>
+            Email:
+          <input type="email" name="email" />
+        </label><br />
+          <label>
+            Password:
+          <input type="password" name="psw" />
+        </label><br />
+          <input type="submit" value="Submit" />
+        </form><br />
+
+        <form>
+           <h1>Sign-up</h1>
+          <label>
+            Family Name:
+          <input type="text" name="familyname" />
+        </label><br />
+          <label>
+            Name:
+          <input type="text" name="name" />
+        </label><br />
+          <label>
+            Email:
+          <input type="email" name="email" />
+        </label><br />
+          <label>
+            Age:
+          <input type="number" name="age" />
+        </label><br />
+          <label>
+            Password:
+          <input type="password" name="psw" />
+        </label><br />
+          <label>
+            Confirm Password:
+          <input type="password" name="psw" />
+        </label><br/>
+          <input type="submit" value="Submit" />
+        </form>
       </div>
     );
   }
