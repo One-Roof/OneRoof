45b42aae558067c169bfee9e0309cf1ae4c6241d
Matt McDonald <mattmcd1221@gmail.com>
2017-08-27 10:08:58 -0400

    test message

--- src/Components/Messages/MessageList.jsx
+++ src/Components/Messages/MessageList.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import OneMessage from './OneMessage';
 import '../../Styles/PostList.css';
 
-class PostList extends Component {
+class MessageList extends Component {
   constructor(props) {
     super(props);
 
@@ -38,4 +38,57 @@ class PostList extends Component {
   }
 }
 
-export default PostList;
+export default MessageList;
+
+// class MessageList extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       messageList: [],
+//     };
+//   }
+//
+//   componentDidMount() {
+//     console.log("message fetched");
+//     fetch("")
+//       .then(results => {
+//         return results.json();
+//       })
+//       .then(data => {
+//         this.setState({messageList: data})
+//       })
+//   }
+//
+//   render() {
+//     return (
+//       <div className="">
+//         <form>
+//           <label>
+//             Title:
+//             <input className="titleTextBox"type="text" name="name"/>
+//           </label>
+//           <br/>
+//           <label>
+//             Message:
+//             <input className="messageTextBox" type="text" />
+//           </label>
+//           <br/>
+//           <input type="Submit" value="Submit"/>
+//         </form>
+//         <div className="">
+//           <h2>Recent Posts:</h2>
+//         </div>
+//         <button type="submit">
+//           Refresh
+//         </button>
+//         <div>
+//           <OneMessage
+//             messageList={this.state.messageList}
+//             />
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+//
+// export default MessageList;
--- src/Components/Messages/OneMessage.jsx
+++ src/Components/Messages/OneMessage.jsx
@@ -1,27 +1,55 @@
 import React, { Component } from 'react';
 
-class OnePost extends Component {
+class OneMessage extends Component {
   render() {
-    let posts = (post => {
+    let oneMessage = (message => {
       return (
         <div className="">
           <div className="">
             <p>User</p>
           </div>
           <div>
-            <p>Post Content</p>
+            <p>Message Content</p>
           </div>
         </div>
       );
     })
     return (
       <div>
-        {posts}
+        {oneMessage}
       </div>
     )
   }
 }
 
-export default OnePost;
+export default OneMessage;
 
-// this.props.posts.map
+
+//Test//
+
+// class OneMessage extends Component {
+//   constructor(props) {
+//     super(props);
+//   }
+//   render() {
+//     let oneMessage = this.props.messageList.results.map(message => {
+//       return (
+//         <div className="" key={message.id}>
+//           <div className="">
+//             <p>User</p>
+//           </div>
+//           <div>
+//             <p>Message Content: {message.content}</p>
+//           </div>
+//         </div>
+//       );
+//     })
+//     return (
+//       <div>
+//         {oneMessage}
+//       </div>
+//     )
+//   }
+// }
+//
+// export default OneMessage;
