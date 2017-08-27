82c97206c6a9f69aaf5ca011ca8b65840f5decf2
Matt McDonald <mattmcd1221@gmail.com>
2017-08-27 16:09:05 -0400

    got messages to display

--- src/Components/Messages/MessageList.jsx
+++ src/Components/Messages/MessageList.jsx
@@ -2,10 +2,25 @@ import React, { Component } from 'react';
 import OneMessage from './OneMessage';
 import '../../Styles/PostList.css';
 
+
 class MessageList extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      messageList: [],
+    };
+  }
 
+  componentDidMount() {
+    console.log("message fetched");
+    fetch("https://thawing-wave-85503.herokuapp.com/api/messages")
+      .then(results => results.json())
+      .then(response => {
+        let messageList = response._embedded.messages
+        this.setState({
+          messageList: messageList
+        })
+      })
   }
 
   render() {
@@ -31,7 +46,9 @@ class MessageList extends Component {
           Refresh
         </button>
         <div>
-          <OneMessage />
+          <OneMessage
+            messageList={this.state.messageList}
+            />
         </div>
       </div>
     );
@@ -39,56 +56,3 @@ class MessageList extends Component {
 }
 
 export default MessageList;
-
-// class MessageList extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       messageList: [],
-//     };
-//   }
-//
-//   componentDidMount() {
-//     console.log("message fetched");
-//     fetch("")
-//       .then(results => {
-//         return results.json();
-//       })
-//       .then(data => {
-//         this.setState({messageList: data})
-//       })
-//   }
-//
-//   render() {
-//     return (
-//       <div className="">
-//         <form>
-//           <label>
-//             Title:
-//             <input className="titleTextBox"type="text" name="name"/>
-//           </label>
-//           <br/>
-//           <label>
-//             Message:
-//             <input className="messageTextBox" type="text" />
-//           </label>
-//           <br/>
-//           <input type="Submit" value="Submit"/>
-//         </form>
-//         <div className="">
-//           <h2>Recent Posts:</h2>
-//         </div>
-//         <button type="submit">
-//           Refresh
-//         </button>
-//         <div>
-//           <OneMessage
-//             messageList={this.state.messageList}
-//             />
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-//
-// export default MessageList;
